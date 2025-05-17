547f1eb134cd2c6ba42e8b994c0dbb7304529fe7
Zofia Nora <sophienora92@gmail.com>
2025-05-17 09:23:25 +0200

    changed App.jsx and added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl">My Application</h1>
+    </header>
+  );
+}
+
+export default Header;
