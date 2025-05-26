1993e1c6cb4c2d95cb9839d2e9f1fada446ce7e7
volumia <getehnba@gmail.com>
2025-05-26 03:11:01 +0300

    frontend: Add "@" path alias to Vite config that resolves to ./src

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
+import path from 'path';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   test: {
     include: [
       ...configDefaults.include,
