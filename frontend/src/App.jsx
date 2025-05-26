310e929683cc07ac6f60aad3737d472851cd35d2
r-farooq <rafiafarooq@hotmail.com>
2025-05-26 22:24:27 +0300

    update App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import Footer from "./components/Footer/footer";
 import ResourceList from "./components/Resources/ResourceList";
 
