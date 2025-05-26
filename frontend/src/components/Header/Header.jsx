3d140deb622466380baaf745f5786fb4ed3a5ef9
r-farooq <rafiafarooq@hotmail.com>
2025-05-26 22:36:30 +0300

    move CSS to Header.module and update classNames with correct syntax

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,23 +1,24 @@
+import styles from './Header.module.css';
+
+
 function Header() {
 
   return (
-    <header className="header">
-        <div className="container header-bar">
-            <p className="logo-name">AppName</p>
+    <header className={styles.header}>
+       <div className={`${styles.container} ${styles.header_bar}`}>
+            <p className={styles.logo_name}>AppName</p>
         </div>
-        <div className="container header-mid">
+        <div className={`${styles.container} ${styles.header_mid}`}>
             <h1>Welcome to AppName </h1>
             <p>A collection of over 200 resources for Developers and Designers, all in one place</p>
         </div>
-        <div className="container input-container">
+        <div className={`${styles.container} ${styles.inputContainer}`}>
             <input
                 type="text"
                 placeholder="Search..."
-                className="search-bar"
+                className={styles.search_bar}
             />
         </div>
-
-      
     </header>
   );
 }
