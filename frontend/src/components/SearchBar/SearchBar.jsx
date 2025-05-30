bb1eff1902e64f1223e96a64cd0c1d86a9f5604b
Mohamed Nabil <medonapil94@gmail.com>
2025-05-30 15:16:59 +0300

    feat: Enhance SearchBar component with dynamic input handling and clear button

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import styles from './SearchBar.module.css';
 
 function SearchBar() {
 
+
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-        <div className= {styles.input_container}>
-            <input
-                type="text"
-                placeholder="Search..."
-                className={styles.search_bar}
-            />
-        </div>
+    <div className={styles.input_container}>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        className={styles.search_bar}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {searchTerm && (
+        <button
+          className={styles.remove_icon}
+          onClick={() => setSearchTerm('')}
+        >
+          ×
+        </button>
+      )}
+
+    </div>
   );
 }
 
