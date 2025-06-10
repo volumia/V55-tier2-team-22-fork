726444fe8fedc20e49bf004d7e5f69e42a71911d
volumia <getehnba@gmail.com>
2025-06-11 00:00:28 +0300

    fe: Add jsdoc comments to the new hooks

--- frontend/src/hooks/usePaginator.js
+++ frontend/src/hooks/usePaginator.js
@@ -1,7 +1,14 @@
 import { computeRangeFromPageIndex } from "@/util/pagination";
 import { useState } from "react";
-computeRangeFromPageIndex;
 
+/**
+ * @typedef {Object} PaginationReturn
+ * @property {{start: number, end: number}} itemDisplayRange - The range of items displayed in the current page. `start` is inclusive, `end` is exclusive.
+ * @property {(index: number) => void} goToPage - Changes `itemDisplayRange` to match the specified page.
+ *
+ * @param {number} pageSize - The maximum amount of items contained in one page.
+ * @returns {PaginationReturn}
+ */
 export function usePaginator(pageSize) {
   const [itemDisplayRange, setItemDisplayRange] = useState(computeRangeFromPageIndex(0, pageSize));
 
