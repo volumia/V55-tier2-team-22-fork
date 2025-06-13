269eda11abc21bf7a78dd31b134083a496087197
volumia <getehnba@gmail.com>
2025-06-14 01:21:35 +0300

    fe: Alter `getResourceData.js` so it fetches data from an external server (it previously retrieved the data from local files)

--- frontend/src/util/getResourceData.js
+++ frontend/src/util/getResourceData.js
@@ -1,11 +1,3 @@
-// Vite has native support for importing and parsing JSONs from local files.
-import resources from "@/assets/data/resources.json";
-import tags from "@/assets/data/tags.json";
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 /**
  * @typedef Tag
  * @property {string} tag
@@ -20,28 +12,12 @@ function sleep(ms) {
  * @property {string} id
  */
 
-/*
- The idea here behind these two functions (getResources and getTags) is to encapsulate 
- the resource fetching code.
- 
- Any code that needs resource data should call these functions instead of calling fetch(). 
- That way, the code doesn't have to care where the resource data is coming from, just that it gets the data. 
- 
- This is useful for because we can easily change how and where we're getting the resource data without 
- having to reprogram other parts of the app. To put it specifically, even though the proxy server is not finished 
- and usable yet, we can fake/mock its functionality, and work on other features as if it is finished.
-
- Simply put:
- Since the proxy server is not finished, these functions return data stored in local files.
- After the proxy server is finished, we can update these functions to fetch from the proxy server instead.
-*/
-
 /**
  * @returns {Resource[]}
  */
 export async function getResources() {
-  // Sleep for a few milliseconds to simulate server response time
-  await sleep(1000);
+  const response = await fetch(`${import.meta.env.VITE_DATA_API_URL}/resources`);
+  const resources = await response.json();
   return resources;
 }
 
@@ -49,7 +25,7 @@ export async function getResources() {
  * @returns {Tag[]}
  */
 export async function getTags() {
-  // Sleep for a few milliseconds to simulate server response time
-  await sleep(1000);
+  const response = await fetch(`${import.meta.env.VITE_DATA_API_URL}/tags`);
+  const tags = await response.json();
   return tags;
 }
