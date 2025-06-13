3ac6dcecd9dc230896476ade70f145e202beff35
volumia <getehnba@gmail.com>
2025-06-14 01:19:15 +0300

    be: Enable CORS for all server routes (it's a simple server so no worries ¯\_(ツ)_/¯)

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import serverless from "serverless-http";
 import getResources from "#src/routes/getResources";
 import getTags from "#src/routes/getTags";
 
 const app = express();
+app.use(cors());
 app.use("/api/resources", getResources);
 app.use("/api/tags", getTags);
 
