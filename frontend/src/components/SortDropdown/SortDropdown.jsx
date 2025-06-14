6700e6e5b8c2d080b7a21b367f97e2ec5b5e6485
volumia <getehnba@gmail.com>
2025-06-14 03:29:41 +0300

    fe: Add feature where clicking out of the SortDropdown will close it

--- frontend/src/components/SortDropdown/SortDropdown.jsx
+++ frontend/src/components/SortDropdown/SortDropdown.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./SortDropdown.module.css";
 import { BiChevronDown } from "react-icons/bi";
 import { BiChevronUp } from "react-icons/bi";
@@ -10,6 +10,7 @@ import { GoSortDesc } from "react-icons/go";
 
 function SortDropdown({ onSortChange, sortBy = "title", sortOrder = "asc" }) {
   const [showOptions, setShowOptions] = useState(false);
+  const dropdownEl = useRef(null);
 
   const handleSortField = (field) => {
     onSortChange(field, sortOrder);
@@ -19,21 +20,31 @@ function SortDropdown({ onSortChange, sortBy = "title", sortOrder = "asc" }) {
     onSortChange(sortBy, order);
   };
 
+  const onDropdownFocusOut = (e) => {
+    // Close dropdown container when clicking or tabbing out of it
+    if (!dropdownEl.current.contains(e.relatedTarget)) {
+      setShowOptions(false);
+    }
+  };
+
+  useEffect(() => {
+    // Focus dropdown container when it mounts.
+    // This allows clicking out of it to be considered a blur event
+    // without having to click *inside it* first to register as such.
+    if (showOptions) dropdownEl.current.focus();
+  }, [showOptions]);
+
   return (
     <div className={styles.container}>
       <button className={styles.sort_button} onClick={() => setShowOptions((prev) => !prev)}>
         <TbArrowsSort size="1.25em" />
         <span className="ms-2">Sort</span>
 
-        {showOptions ? (
-          <BiChevronUp size="1.5em" />
-        ) : (
-          <BiChevronDown size="1.5em" />
-        )}
+        {showOptions ? <BiChevronUp size="1.5em" /> : <BiChevronDown size="1.5em" />}
       </button>
 
       {showOptions && (
-        <div className={styles.sortOptions}>
+        <div ref={dropdownEl} className={styles.sortOptions} tabIndex="-1" onBlurCapture={onDropdownFocusOut}>
           <h3 className={styles.section_header}>Sort by</h3>
 
           <button
