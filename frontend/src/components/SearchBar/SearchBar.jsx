2b7de3b697b6ab8354e125845507df2a67b4feb4
volumia <getehnba@gmail.com>
2025-06-14 04:16:41 +0300

    fe: Make search bar appearance closer to UI mockup. Also improve responsiveness of thinner screens

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,32 +1,35 @@
 import { IoSearchOutline } from "react-icons/io5";
 import styles from "./SearchBar.module.css";
 
-function SearchBar({
-  searchTerm,
-  searchFilter,
-  setSearchTerm,
-  setSearchFilter,
-  onClearAll
-}) {
+function SearchBar({ searchTerm, searchFilter, setSearchTerm, setSearchFilter, onClearAll }) {
   return (
-    <div className="max-w-screen">
-      <div className="relative mx-auto w-max grid grid-col-1 sm:flex justify-between gap-x-5 items-center">
-        <div className="flex items-center">
-          <div className="absolute inset-y-0 left-0 bottom-24 sm:bottom-0 pl-4 flex items-center pointer-events-none">
-            <IoSearchOutline className="md:left-5 md:top-5 md:h-5 md:w-5 md:ml-3 text-gray-400" />
-          </div>
+    <div>
+      <div className="relative flex justify-center items-center gap-x-5 mx-auto w-full">
+        <div
+          className="
+            relative flex flex-row items-center 
+            w-lg max-w-full
+            ps-2
+            bg-mono-50
+            border-1 border-mono-400 rounded-full 
+            text-mono-950
+            has-focus:outline-1 has-focus:outline-cyan-300 has-focus:border-cyan-300
+            transition
+          "
+        >
+          <IoSearchOutline size="1.5em" className="absolute" />
 
           <input
             type="text"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="Search..."
-            className="outline-2 lg:w-xl outline-cyan-400 rounded-2xl py-2 px-4 md:ml-5 pl-8 focus:outline-none focus:ring-2 focus:ring-cyan-900 transition"
+            className="w-full ps-8 pe-3 py-2 placeholder:text-mono-700 outline-none"
           />
         </div>
 
         <select
-          className="border rounded p-2 text-black bg-white my-2.5 outline-0 cursor-pointer"
+          className="border-1 border-mono-400 rounded p-2 text-black bg-white my-2.5 outline-0 cursor-pointer"
           value={searchFilter}
           onChange={(e) => setSearchFilter(e.target.value)}
         >
